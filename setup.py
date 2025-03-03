386c164d62a2df390684a99890ac917fe632db40
Qiuqiang Kong <qiuqiangkong@gmail.com>
2025-03-03 23:53:50 +0800

    v0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="audidata", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="Qiuqiang Kong",
     author_email="qiuqiangkong@gmail.com",
     description="Audio dataloaders",
